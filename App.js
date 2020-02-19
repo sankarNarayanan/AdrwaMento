1feda204f0b16f47e0d944ce22ed19fed7cc199b
Sankar Narayanan <sankar.narayanan@emc.com>
2020-02-19 17:48:52 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/getLinkedinUser', function(req,res){
+    code = req.query.code;
+    var request = require('request');
+    url = 'https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code='+code+'&redirect_uri=http://localhost:3000/linkedInRedirect&client_id=81y0p9iz2huzbk&client_secret=RYPYWae99eMjAaDR';
+    var options = {
+    'method': 'POST',
+    'url': url,
+    'headers': {
+  }
+};
+request(options, function (error, response) { 
+  if (error) throw new Error(error);
+  console.log(response.body);
+  res.send(response.body);
+});
+
+
+});
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
+});
+
