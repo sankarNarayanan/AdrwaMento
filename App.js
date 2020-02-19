4d7f5ff1a47cdcb58261461a19796c6cafa0b280
Sankar Narayanan <sankar.narayanan@emc.com>
2020-02-19 18:53:13 +0530

    enable cors.

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 const port = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
