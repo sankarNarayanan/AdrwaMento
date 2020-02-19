3e9c4e1c8dfd67b8b869534f649b539d1cc165c5
Sankar Narayanan <sankar.narayanan@emc.com>
2020-02-19 18:44:29 +0530

    linked in auth completed.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -18,11 +18,34 @@ app.get('/getLinkedinUser', function(req,res){
 };
 request(options, function (error, response) { 
   if (error) throw new Error(error);
-  res.send(response.body);
+  console.log(response.body);
+  var responseObj = JSON.parse(response.body);
+  if(responseObj.hasOwnProperty('access_token')){
+     getUserEmail(responseObj['access_token'], res);
+    }else{
+    res.send(response.body);
+    }
 });
 });
 
 app.listen(port, function () {
   console.log('Example app listening on port 3001!');
 });
+
+function getUserEmail(accessToken, res){
+    console.log('email method called');
+    var request = require('request');
+    var options = {
+    'method': 'GET',
+    'url': 'https://api.linkedin.com/v2/clientAwareMemberHandles?q=members&projection=(elements*(primary,type,handle~))',
+    'headers': {
+        'Authorization': 'Bearer '+accessToken
+    }
+    };
+    request(options, function (error, response) { 
+    if (error) throw new Error(error);
+    res.send(response.body);
+    });
+
+}
 
